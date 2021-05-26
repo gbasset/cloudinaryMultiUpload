075fdb1039c3ce31708a95930d0e99d1e3df5893
Gaetan basset <gbasset@protonmail.com>
2021-05-26 10:21:48 +0200

    add new api search type

--- server/server.js
+++ server/server.js
@@ -9,56 +9,66 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 
-app.get('/api/images', async (req, res) => {
+// Get images in a client folder with id
+app.get('/api/images/:clientId/:folderId', async (req, res) => {
+    const clientId = req.params.clientId;
+    const folderId = req.params.folderId;
     const { resources } = await cloudinary.search
-        .expression('folder:dev_setups')
+        .expression(`exact folder:simplify-dev/${clientId}/${folderId}`)
         .sort_by('public_id', 'desc')
         .max_results(30)
         .execute();
-    // const publicIds = resources.map((file) => file.public_id);
-    res.send(resources);
+    res.json(resources);
 });
+// upload image in a client folder in folderId
 app.post('/api/upload', async (req, res) => {
     try {
         const fileStr = req.body.data;
         const clientId = req.body.clientId;
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+        const folderId = req.body.folderId;
+        const eager = req.body.eager;
+        await cloudinary.uploader.upload(fileStr, {
             upload_preset: 'dev_setups',
-            folder: `client/${clientId}`,
+            folder: `simplify-dev/${clientId}/${folderId}`,
             use_filename: true,
-            unique_filename: false
-        });
-        console.log(uploadResponse);
-        res.json({ msg: 'yaya' });
+            unique_filename: false,
+            eager: eager
+        },
+            function (error, result) {
+                res.json(result);
+            });
     } catch (err) {
         console.error(err);
         res.status(500).json({ err: 'Something went wrong' });
     }
 });
-/// créé le dossier clien avec id 
-// ensuite j'upload avec un autre dossier dans ce dossier avec id de la librairie créé 
-// ensuite je copie les liens vers la librairie beyable
-// redimensionnement 
+
+// Create a root folder for client
 app.post('/api/create', async (req, res) => {
     const clientId = req.body.clientId;
     try {
         cloudinary.api.create_folder(
-            `client/${clientId}`,
+            `simplify-dev/${clientId}`,
             function (error, result) {
-                console.log(result);
                 res.json({ msg: result });
             });
     } catch (err) {
         console.error(err);
         res.status(500).json({ err: 'Something went wrong' });
     }
-
 });
-// get presets
-app.get('/api/presets', async (req, res) => {
-    const resources = await cloudinary.api.root_folders((err, result) => {
-        res.json(result)
-    });
+// get presets of folder
+app.get('/api/presets/:clientId', async (req, res) => {
+    const clientId = req.params.clientId;
+    let dataInfos = [];
+    cloudinary.api.root_folders((err, result) => {
+        dataInfos.push({ account: result })
+    }).then(
+        cloudinary.api.sub_folders(`simplify-dev/${clientId}`, (err, result) => {
+            dataInfos.push({ client: result })
+            res.json(dataInfos)
+        })
+    );
 });
 
 const port = process.env.PORT || 3001;
